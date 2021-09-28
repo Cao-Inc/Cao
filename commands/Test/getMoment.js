b011f04ea8fa0c4cbe9f7216613c1589cedd5067
KiritoU <nguyentienduongtink21@gmail.com>
2021-09-28 08:45:07 +0700

    test timezone

--- commands/Test/getMoment.js
+++ commands/Test/getMoment.js
@@ -1,6 +1,8 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const moment = require('moment-timezone');
 
+moment.tz.setDefault('Asia/Vietnam');
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('moment')
